f763205dcbf398205ffccd97f86577d542136eed
jaimesHub <duongquockhanh230596@gmail.com>
2024-07-25 23:42:06 +0700

    #70 - Config Axios Interceptor (response)

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,6 +1,7 @@
-import { Button, Input } from "antd";
+import { Button, Input, notification } from "antd";
 import { useState } from "react";
-import axios from "axios";
+import { createUserAPI } from "../../services/api.service";
+
 
 const UserForm = () => {
     const [fullName, setFullName] = useState("");
@@ -8,16 +9,28 @@ const UserForm = () => {
     const [password, setPassword] = useState("");
     const [phone, setPhone] = useState("");
 
-    const handleClickBtn = () => {
-        const URL_BACKEND = "http://localhost:8080/api/v1/user";
-        const data = {
-            fullName: fullName,
-            email: email,
-            password: password,
-            phone: phone
-        };
-        axios.post(URL_BACKEND, data);
-        console.log(">>> check info: ", { fullName, email, password, phone });
+    const handleClickBtn = async () => {
+        // console.log(">>> check state: ", { fullName, email, password, phone });
+        const res = await createUserAPI(fullName, email, password, phone);
+        // if (res.data && res.data.data) {
+        //     notification.success({
+        //         message: "Create a new user",
+        //         description: "Tạo mới người dùng thành công"
+        //     });
+        // }
+
+        // console.log(">>> check res: ", res.data.data);
+
+        console.log(">>> check res (After adding interceptors): ", res);
+        if (res.data) {
+            // After adding interceptors at axios.customize.js file
+            notification.success({
+                message: "Create a new user",
+                description: "Tạo mới người dùng thành công"
+            });
+        }
+
+        console.log(">>> check res (After adding interceptors): ", res.data);
     }
 
     return (
