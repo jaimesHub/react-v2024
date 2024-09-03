9993791c57a6ca344cbad609c608167f75e69464
jaimesHub <duongquockhanh230596@gmail.com>
2024-09-03 18:48:16 +0700

    #91. Responsive Layout in Antd

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,4 +1,4 @@
-import { Button, Input, Form, notification } from "antd";
+import { Button, Input, Form, notification, Row, Col } from "antd";
 import { registerUserAPI } from "../services/api.service";
 
 import { useNavigate } from "react-router-dom";
@@ -40,85 +40,96 @@ const RegisterPage = () => {
             form={form}
             layout="vertical"
             onFinish={onFinish}
-        // onFinishFailed={onFinishFailed}
+            style={{
+                margin: "10px",
+            }}
         >
-            <div style={{
-                margin: "50px",
-            }}>
-                <Form.Item
-                    label="Full Name"
-                    name="fullName"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your full name!',
-                        },
-                    ]}
-                >
-                    <Input />
-                </Form.Item>
-
-                <Form.Item
-                    label="Email"
-                    name="email"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your email!',
-                        },
-                    ]}
-                >
-                    <Input />
-                </Form.Item>
-
-                <Form.Item
-                    label="Password"
-                    name="password"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your password!',
-                        },
-                    ]}
-                >
-                    <Input.Password />
-                </Form.Item>
-
-                <Form.Item
-                    label="Phone number"
-                    name="phone"
-                    rules={[
-                        {
-                            // required: true,
-                            pattern: new RegExp(/\d+/g),
-                            message: "Wrong format!"
-                        }
-                    ]}
-                >
-                    <Input />
-                </Form.Item>
-
-
-                <div>
-                    <Button
-                        onClick={() => form.submit()}
-                        type="primary"
+            <Row justify={"center"}>
+                <Col xs={24} md={8}>
+                    <h1 style={{ textAlign: "center" }}>Register</h1>
+                </Col>
+            </Row>
+            <Row justify={"center"}>
+                <Col xs={24} md={8}>
+                    <Form.Item
+                        label="Full Name"
+                        name="fullName"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your full name!',
+                            },
+                        ]}
                     >
-                        Register
-                    </Button>
-                    <Button
-                        onClick={() => {
-                            console.log(">>> check form:: ", form.getFieldsValue());
-                            form.setFieldsValue({
-                                email: "testtt@gmail.com",
-                                fullName: "testtt"
-                            });
-                        }}
+                        <Input />
+                    </Form.Item>
+                </Col>
+            </Row>
+
+            <Row justify={"center"}>
+                <Col xs={24} md={8}>
+                    <Form.Item
+                        label="Email"
+                        name="email"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your email!',
+                            },
+                        ]}
                     >
-                        Test Autofill Username to Email Field
-                    </Button>
-                </div>
-            </div>
+                        <Input />
+                    </Form.Item>
+                </Col>
+            </Row>
+
+            <Row justify={"center"}>
+                <Col xs={24} md={8}>
+                    <Form.Item
+                        label="Password"
+                        name="password"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your password!',
+                            },
+                        ]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+                </Col>
+            </Row>
+
+            <Row justify={"center"}>
+                <Col xs={24} md={8}>
+                    <Form.Item
+                        label="Phone number"
+                        name="phone"
+                        rules={[
+                            {
+                                // required: true,
+                                pattern: new RegExp(/\d+/g),
+                                message: "Wrong format!"
+                            }
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+                </Col>
+            </Row>
+
+            <Row justify={"center"}>
+                <Col xs={24} md={8}>
+                    <div>
+                        <Button
+                            onClick={() => form.submit()}
+                            type="primary"
+                        >
+                            Register
+                        </Button>
+                    </div>
+                </Col>
+            </Row>
         </Form>
     );
 }
